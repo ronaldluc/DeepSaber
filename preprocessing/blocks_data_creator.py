11cffd8ff625449a2471b7c897993936ef1cf1ab
Jan Pokorný <jenompokorny@gmail.com>
2019-02-23 14:34:56 +0100

    Fix warning

--- preprocessing/blocks_data_creator.py
+++ preprocessing/blocks_data_creator.py
@@ -9,13 +9,14 @@ def generate_blocks(path: str, difficulties: List[str]):
     difficulties = [f'{d}.json' for d in difficulties]
     stats = {'processed': 0, 'failed': 0}
 
-    for root, dirs, files in os.walk(path, topdown=False):
+    for root, _, files in os.walk(path, topdown=False):
         for name in files:
             if name in difficulties:
                 try:
                     df = json_to_blockmask(os.path.join(root, name))
                     os.makedirs(os.path.join(root, 'blocks'), exist_ok=True)
-                    df.to_pickle(os.path.join(root, 'blocks', f'{name[:-5]}.pkl'))
+                    df.to_pickle(os.path.join(
+                        root, 'blocks', f'{name[:-5]}.pkl'))
                 except KeyError as e:
                     stats['failed'] += 1
                     print(f'Failed on {root}/{name} | {str(e)[:20]}')
@@ -23,6 +24,7 @@ def generate_blocks(path: str, difficulties: List[str]):
                 stats['processed'] += 1
 
     pprint(stats)
+
 
 if __name__ == '__main__':
     generate_blocks('../data', ['Hard', 'Expert', 'ExpertPlus'])
